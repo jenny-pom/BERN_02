fb53ba260d932c012e9b6c136482ae1ecfbeb81f
pompom <je3384la-s@student.lu.se>
2026-09-06 17:51:54 +0200

    Final version of Assigment 2

---
+++ Exercise_2/scripts/exercise_2.py
@@ -0,0 +1,25 @@
+import pandas as pd # for data manipulation
+import numpy as np # for data manipulation
+import matplotlib.pyplot as plt # for plotting
+import seaborn as sns # for plotting
+
+# Load bird data
+bird_df = pd.read_csv("../data/bird_count.csv")
+bird_df_sorted = bird_df.sort_values(['yr'])
+print(bird_df_sorted)
+
+def possion_regression():
+
+    return()
+
+def optimizations_of_parameters():
+
+    return()
+
+def maximum_likelihood():
+
+    return()
+
+def output_csv():
+
+    return()
